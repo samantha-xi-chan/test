fe6e8d073c462d96c1166541a917f36e15d34a8c
samantha-xi-chan <142402116+samantha-xi-chan@users.noreply.github.com>
2023-08-31 19:07:32 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,14 @@ echo "$1: "$1
 echo "$2: "$2
 echo "$3: "$3
 
+sleep 6
 ls -alh /docker/download/
 
 cat  /docker/download/out.txt >> /docker/built/out.txt
 echo "build finished " >> /docker/built/out.txt
 
 echo ; echo ; echo ;
+
+
+sleep 6
+ls -alh /docker/built/
