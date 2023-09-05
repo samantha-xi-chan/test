adfe499ddb5e4bf1c846725e85711744633ed589
samantha-xi-chan <142402116+samantha-xi-chan@users.noreply.github.com>
2023-09-05 11:09:30 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 
 echo " = = =  step build.sh = = = "
-VERSION="20230904 # 18:27"
+VERSION="20230905 # 11:27"
 echo "VER: "$VERSION
 
 echo '$1: '$1
 echo '$2: '$2
 echo '$3: '$3
 
-sleep 2
+sleep 1
 ls -alh /docker/download/
 
 cat  /docker/download/out.txt >> /docker/built/out.txt
@@ -16,5 +16,5 @@ echo "build finished " >> /docker/built/out.txt
 echo ; 
 # echo ; echo ;
 
-sleep 2
+sleep 1
 ls -alh /docker/built/
