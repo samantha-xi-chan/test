2b96543477e19481e78c6fad70ef63719b8e4b13
samantha-xi-chan <142402116+samantha-xi-chan@users.noreply.github.com>
2023-09-05 11:09:00 +0800

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,6 +1,6 @@
 
 echo " = = =  step clone.sh  = = =  "
-VERSION="20230904 # 18:27"
+VERSION="20230905 # 11:27"
 echo "VER: "$VERSION
 
 
@@ -8,7 +8,7 @@ echo '$1: '$1
 echo '$2: '$2
 echo '$3: '$3
 
-sleep 60
+sleep 1
 echo "msg from clone.sh" >> /docker/download/out.txt
 
 # exit 66
